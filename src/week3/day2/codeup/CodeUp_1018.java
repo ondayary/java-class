d2ed6933a28e3ae260ad62df9b24816f2d725ba7
ondayary@gmail.com <ondayary@gmail.com>
2022-10-06 01:44:24 +0900

    시간 입력받아 출력하기

---
+++ src/week3/day2/codeup/CodeUp_1018.java
@@ -0,0 +1,13 @@
+package week3.day2.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp_1018 {
+    // 시(hour)와 분(minute)이 ":" 으로 구분되어 입력되면
+    // "시:분" 형식으로 출력
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String time = sc.next();
+        System.out.printf("%s", time);
+    }
+}
