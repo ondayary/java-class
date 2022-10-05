ecd144570e40b78da5db56e16ed2fbfc1d74b5d9
ondayary@gmail.com <ondayary@gmail.com>
2022-10-06 01:36:30 +0900

    실수 입력받아 둘째 자리까지 출력하기

---
+++ src/week3/day2/codeup/CodeUp_1015.java
@@ -0,0 +1,12 @@
+package week3.day2.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp_1015 {
+    // 결과를 소수점 셋째 자리에서 반올림 해, 소숫점 이하 둘째 자리까지 출력
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float num = sc.nextFloat();
+        System.out.printf("%.2f", num);
+    }
+}
