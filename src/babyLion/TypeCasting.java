866f8b35d998cee9567de7b39bc777c706be07b2
ondayary@gmail.com <ondayary@gmail.com>
2022-10-06 01:23:37 +0900

    221004 배열, 반복문 복습

---
+++ src/babyLion/TypeCasting.java
@@ -0,0 +1,21 @@
+package babyLion;
+
+public class TypeCasting {
+    public void divide(int first, int second) {
+        float fFirst = (float)first;
+        float fSecond = (float)second;
+        float result = fFirst / fSecond;
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        TypeCasting tc = new TypeCasting();
+        tc.divide(2, 3);
+
+        // int(Integer)는 소수점이 없다.
+        // TypeCasting Type(형)이 바뀐다. => 형변환
+        // int first = 2;
+        // int second = 3;
+        // System.out.println(first / second);
+    }
+}
