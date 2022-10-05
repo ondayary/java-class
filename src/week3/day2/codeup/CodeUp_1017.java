b15fa33ed9c68d507fb8d0d13420f88c2edf20be
ondayary@gmail.com <ondayary@gmail.com>
2022-10-06 01:36:42 +0900

    정수 1개 입력받아 3번 출력하기

---
+++ src/week3/day2/codeup/CodeUp_1017.java
@@ -0,0 +1,12 @@
+package week3.day2.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp_1017 {
+    // int형 정수 1개를 입력받아 공백을 사이에 두고 3번 출력
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.printf("%d %d %d", num, num, num);
+    }
+}
