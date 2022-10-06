39ab493e457eba8729b8c441cc83fc8b7670cedd
ondayary@gmail.com <ondayary@gmail.com>
2022-10-06 10:17:41 +0900

    221006 List

---
+++ src/week3/day3/collection/ListExercise.java
@@ -0,0 +1,19 @@
+package week3.day3.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListExercise {
+    private List<String> students;
+
+    public ListExercise() {
+        this.students = new ArrayList<>();
+        this.students.add("김민석");
+        this.students.add("한소희");
+        this.students.add("배수지");
+    }
+
+    public List<String> getStudents() {
+        return this.students;
+    }
+}
