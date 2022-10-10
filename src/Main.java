05e605629e79c15aa0fdc067e3967a2f5ee8b43c
ondayary@gmail.com <ondayary@gmail.com>
2022-10-10 20:58:50 +0900

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//        String a = "hello";
+//        String b = "daon";
+//        System.out.println(a + b);
+
+        Main main = new Main(); // 메인 클래스 자기 자신을 선언
+        main.strCombine("java", "main");
+    }
+    // 메소드로 분리해보자
+    public void strCombine(String a, String b) { // 어디선가 메소드를 호출할 때 넣어줄 타입과 변수명을 준비한다.
+        String result = a + b;
+        System.out.println(result);
     }
 }
