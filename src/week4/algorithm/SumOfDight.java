be4e950a697f9d4e75fa4e804cf7488769b58436
ondayary@gmail.com <ondayary@gmail.com>
2022-10-11 10:01:23 +0900

    221011 programmers algorithm

---
+++ src/week4/algorithm/SumOfDight.java
@@ -0,0 +1,21 @@
+package week4;
+
+public class SumOfDight {
+    // 자연수 N이 주어지면, N의 각 자릿수의 합을 구해서 return 하는 solution 함수를 만들어 주세요.
+    // 예를들어 N = 123이면 1 + 2 + 3 = 6을 return 하면 됩니다.
+
+    public int solution(int num) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        SumOfDight sod = new SumOfDight();
+        int result = sod.solution(831);
+
+        if(result == 10){
+            System.out.println("테스트 성공");
+        } else {
+            System.out.printf("테스트 실패 result:%d \n", result);
+        }
+    }
+}
