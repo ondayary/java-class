7001d56e33681ec6207b187346507153ec167129
ondayary@gmail.com <ondayary@gmail.com>
2022-10-17 09:28:02 +0900

    221017 직삼각형 별찍기

--- src/week5/RightTriangle.java
+++ src/week5/RightTriangle.java
@@ -1,10 +1,23 @@
 package week5;
 
 public class RightTriangle {
+    public void Star(int num) {
+        for(int i = 1; i < num; i++) {
+            for(int j = 1; j < i + 1; j++) {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    /*
+    System.out.println("*");        i = 0 별 1개
+    System.out.println("* *");      i = 1 별 2개
+    System.out.println("* * *");    i = 2 별 3개
+    System.out.println("* * * *");  i = 3 별 4개
+    */
     public static void main(String[] args) {
-        System.out.println("*");
-        System.out.println("* *");
-        System.out.println("* * *");
-        System.out.println("* * * *");
+        RightTriangle rightTriangle = new RightTriangle();
+        rightTriangle.Star(5);
     }
 }
