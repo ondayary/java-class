d48f3c838415c3ecd57cf0dc84bac37447edca92
ondayary@gmail.com <ondayary@gmail.com>
2022-10-18 09:27:09 +0900

    221018 repeat 을 이용한 정사각형 별찍기

--- src/week5/SquareStar_repeat.java
+++ src/week5/SquareStar_repeat.java
@@ -1,18 +1,14 @@
 package week5;
 
-public class SquareStar {
+public class SquareStar_repeat {
     // n을 받으면 n * n 사이즈 사각형 출력
     public void printSquare(int num) {
         for(int i = 0; i < num; i++) {
-            for (int j = 0; j < num; j++) {
-                System.out.print(" * ");
-            }
-            System.out.println();
+            System.out.println(" * ".repeat(num));
         }
     }
 
     public static void main(String[] args) {
-        SquareStar squareStar = new SquareStar();
-        squareStar.printSquare(5);
+        new SquareStar_repeat().printSquare(5);
     }
 }
